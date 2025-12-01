bf49c62b0760f39682cb75215d6958357f87133a
PhoXinuS <piotr.szkoda.mail@gmail.com>
2025-12-01 17:42:35 +0100

    Moved templates and created admin subpage

---
+++ app/admin/routes.py
@@ -0,0 +1,41 @@
+from flask import render_template, redirect, url_for, flash
+from flask_login import login_required, current_user
+from app import db
+from app.admin import admin
+from app.models import User, Question
+
+@admin.route('/dashboard')
+@login_required
+def dashboard():
+    if current_user.role != 'admin':
+        return redirect(url_for('main.index'))
+        
+    users = User.query.all()
+    questions = Question.query.all()
+    return render_template('admin.html', users=users, questions=questions)
+
+@admin.route('/delete_user/<int:user_id>')
+@login_required
+def delete_user(user_id):
+    if current_user.role != 'admin': return redirect(url_for('main.index'))
+    
+    user = User.query.get(user_id)
+    if user and user.id != current_user.id:
+        db.session.delete(user)
+        db.session.commit()
+        flash(f'User {user.username} deleted.')
+        
+    return redirect(url_for('admin.dashboard'))
+
+@admin.route('/delete_question/<int:q_id>')
+@login_required
+def delete_question(q_id):
+    if current_user.role != 'admin': return redirect(url_for('main.index'))
+    
+    q = Question.query.get(q_id)
+    if q:
+        db.session.delete(q)
+        db.session.commit()
+        flash('Question deleted.')
+        
+    return redirect(url_for('admin.dashboard'))
