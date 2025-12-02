e28b96c2ecf734c61ac26f2847f6a59122f49a90
PhoXinuS <piotr.szkoda.mail@gmail.com>
2025-12-02 08:58:01 +0100

    Migrated to postgresql

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = 'mvp-secret-key' # Needed for session management
-    # TODO: Change to postgresql
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///mvp.db'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'mvp-secret-key-change-me'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://user:password@localhost:5432/homework_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
