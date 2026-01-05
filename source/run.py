3699f61e73ddebc276814fef4c4535e4705f690f
nolann nougues <n.nougues@hotmail.com>
2026-01-05 12:07:47 +0100

    Modified port to 8080

--- source/run.py
+++ source/run.py
@@ -25,4 +25,5 @@ with app.app_context():
 
 if __name__ == '__main__':
     debug_mode = os.environ.get('FLASK_DEBUG', '0') == '1'
-    app.run(debug=debug_mode, host='0.0.0.0')
+    port = int(os.environ.get('FLASK_PORT', 8080))
+    app.run(debug=debug_mode, host='0.0.0.0', port=port)
